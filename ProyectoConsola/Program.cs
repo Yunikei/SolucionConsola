80e6a68d5caf9b064f7b7674ea57180cbcb9d7b2
Yunkei <alva_alcantar24@unikino.edu.mx>
2024-09-03 07:56:54 -0600

    Creando mi primer variable

--- ProyectoConsola/Program.cs
+++ ProyectoConsola/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hola C#");
+
+            string mIPrimerVariable;
+            mIPrimerVariable = "Hola2";
+            Console.WriteLine(mIPrimerVariable);
         }
     }
 }
